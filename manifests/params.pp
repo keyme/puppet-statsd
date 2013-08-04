c109b164771f4230d2d9091865242c6ea3757ad7
Zachary Salzbank <zach@keyme.net>
2013-08-04 22:06:45 +0000

    make this work

--- manifests/params.pp
+++ manifests/params.pp
@@ -12,17 +12,7 @@ class statsd::params($node_module_dir  = '') {
     'Debian': {
       $init_script = 'puppet:///modules/statsd/statsd-init'
       if ! $node_module_dir {
-        case $provider {
-          'apt': {
-            $statsjs = '/usr/share/statsd/stats.js'
-          }
-          'npm': {
-            $statsjs = '/usr/lib/node_modules/statsd/stats.js'
-          }
-          default: {
-            fail('Unsupported provider')
-          }
-        }
+        $statsjs = '/usr/lib/node_modules/statsd/stats.js'
       } 
       else {
         $statsjs = "${node_module_dir}/statsd/stats.js"
